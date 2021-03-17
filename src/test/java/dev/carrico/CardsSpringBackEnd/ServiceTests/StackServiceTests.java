57ae8fc8843007ffca63c680602b534434a63417
tom <xian.tom.wan@gmail.com>
2021-03-16 21:35:12 -0500

    added Stack and Tag service tests

---
+++ src/test/java/dev/carrico/CardsSpringBackEnd/ServiceTests/StackServiceTests.java
@@ -0,0 +1,64 @@
+package dev.carrico.CardsSpringBackEnd.ServiceTests;
+
+import dev.carrico.entities.Stack;
+import dev.carrico.services.StackService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Set;
+
+@SpringBootTest
+@Transactional
+public class StackServiceTests {
+    @Autowired
+    StackService ss;
+
+    @Test
+    void create_stack(){
+        Stack stack = new Stack();
+        stack.setCreatorId(1);
+        stack.setStackName("Math");
+        ss.createStack(stack);
+
+        Assertions.assertNotEquals(0, stack.getStackId());
+    }
+
+    @Test
+    void get_stack_by_id(){
+        Stack stack = ss.getStackById(1);
+
+        Assertions.assertNotNull(stack);
+        Assertions.assertTrue(stack.getStackName().equals("Basic Math"));
+    }
+
+    @Test
+    void get_all_stacks(){
+        Set<Stack> stacks = ss.getAllStacks();
+
+        Assertions.assertNotNull(stacks);
+        Assertions.assertTrue(stacks.size() > 0);
+    }
+
+    @Test
+    void update_stack(){
+        Stack stack = new Stack();
+        stack.setStackId(1);
+        stack.setStackName("Test Stack");
+        stack.setDescription("Test Description");
+        ss.updateStack(stack);
+        stack = ss.getStackById(1);
+
+        Assertions.assertTrue(stack.getStackName().equals("Test Stack"));
+        Assertions.assertTrue(stack.getDescription().equals("Test Description"));
+    }
+
+    @Test
+    void delete_stack(){
+        boolean result = ss.deleteStackById(1);
+
+        Assertions.assertTrue(result);
+    }
+}
