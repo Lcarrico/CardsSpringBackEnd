cfc30420b43c010800e1bc48fd631e1864ecd86d
Leo Carrico <lcarrico827@gmail.com>
2021-03-16 22:32:11 -0500

    added controllers

---
+++ src/main/java/dev/carrico/controllers/LearnerController.java
@@ -0,0 +1,47 @@
+package dev.carrico.controllers;
+
+import dev.carrico.entities.Learner;
+import dev.carrico.services.LearnerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Set;
+
+@Component
+@RestController
+public class LearnerController {
+    @Autowired
+    LearnerService learnerService;
+
+    @PostMapping("/learners")
+    public Learner createLearner(@RequestBody Learner learner){
+        this.learnerService.createLearner(learner);
+        return learner;
+    }
+
+    @GetMapping("/learners/{learnerId}")
+    public Learner getLearnerById(@PathVariable int learnerId){
+        Learner learner = this.learnerService.getLearnerById(learnerId);
+        return learner;
+    }
+
+    @GetMapping("/learners")
+    public Set<Learner> getAllLearners(){
+        Set<Learner> learners = this.learnerService.getAllLearners();
+        return learners;
+    }
+
+    @PutMapping("/learners/{learnerId}")
+    public Learner updateLearner(@PathVariable int learnerId, @RequestBody Learner learner){
+        learner.setLearnerId(learnerId);
+        this.learnerService.updateLearner(learner);
+        return learner;
+    }
+
+    @DeleteMapping("/learners/{learnerId}")
+    public Boolean deleteBookById(@PathVariable int learnerId){
+        Boolean result = this.learnerService.deleteLearnerById(learnerId);
+        return result;
+    }
+}
---
+++ src/main/java/dev/carrico/controllers/StackController.java
@@ -0,0 +1,47 @@
+package dev.carrico.controllers;
+
+import dev.carrico.entities.Stack;
+import dev.carrico.services.StackService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Set;
+
+@Component
+@RestController
+public class StackController {
+    @Autowired
+    StackService stackService;
+
+    @PostMapping("/stacks")
+    public Stack createStack(@RequestBody Stack stack){
+        this.stackService.createStack(stack);
+        return stack;
+    }
+
+    @GetMapping("/stacks/{stackId}")
+    public Stack getStackById(@PathVariable int stackId){
+        Stack stack = this.stackService.getStackById(stackId);
+        return stack;
+    }
+
+    @GetMapping("/stacks")
+    public Set<Stack> getAllStacks(){
+        Set<Stack> stacks = this.stackService.getAllStacks();
+        return stacks;
+    }
+
+    @PutMapping("/stacks/{stackId}")
+    public Stack updateStack(@PathVariable int stackId, @RequestBody Stack stack){
+        stack.setStackId(stackId);
+        this.stackService.updateStack(stack);
+        return stack;
+    }
+
+    @DeleteMapping("/stacks/{stackId}")
+    public Boolean deleteBookById(@PathVariable int stackId){
+        Boolean result = this.stackService.deleteStackById(stackId);
+        return result;
+    }
+}
