8b1c48cd9e680a38b3f8aadf7d26014b3030cab9
tom <xian.tom.wan@gmail.com>
2021-03-28 13:29:40 -0500

    added changes for https-security

---
+++ src/main/java/dev/carrico/configs/SecurityConfig.java
@@ -0,0 +1,19 @@
+package dev.carrico.configs;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .cors().and()
+                .requiresChannel()
+                .anyRequest()
+                .requiresSecure()
+                .and().csrf().disable();
+    }
+}
